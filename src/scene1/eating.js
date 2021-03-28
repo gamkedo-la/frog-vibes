f155d86f92bc4469a0f77624ae347ddace31125e
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2021-03-28 20:02:16 +0800

    Different tongue when missing a bug.

--- src/scene1/eating.js
+++ src/scene1/eating.js
@@ -6,69 +6,105 @@ import { addUpdate } from "../scripts/mainLoop";
 //AUDIO
 var track = new Track("public/audio/track1.wav", [10, 12, 14, 16], "public/audio/scene01/kerop.wav");
 var timePerFrame = 416.66;
-//SPRITE
+
+// SCENE SPRITE
 import testimg2 from "./img/frog.png";
 const spriteThing = new Sprite(testimg2, 2, 1);
 spriteThing.Stop();
 
-// TONGUE
-import tongue from "./img/frog_tongue.png";
-const spriteTongue = new Sprite(tongue, 1, 1);
-spriteTongue.Stop();
+// TONGUE VALUES
 const TONGUE_X = 100;
 const TONGUE_Y = 34;
 const TONGUE_EXTENDS_Y = 14;
 const TONGUE_EXTENDS_Y_SPEED = 6;
-spriteTongue.x = TONGUE_X;
-spriteTongue.y = TONGUE_Y;
-var isTongueStretchedHit = false;
-
-// TONGUE BODY
-import tongueBody from "./img/frog_tongue_body.png";
-const spritesTongueBody = [
-  new Sprite(tongueBody, 1, 1),
-  new Sprite(tongueBody, 1, 1)
-];
 const TONGUE_BODY_Y = 64;
 const TONGUE_BODY_HEIGHT = 23;
 const TONGUE_BODY_EXTENDS_Y_SPEED = 6;
-spritesTongueBody.map((spriteTongueBody, i) => {
-  spriteTongueBody.Stop();
-  spriteTongueBody.x = TONGUE_X;
-  spriteTongueBody.y = TONGUE_BODY_Y + TONGUE_BODY_HEIGHT * i;
-});
+let isTongueStretched = false;
+let isTongueStretchedHit = false;
 
+// TONGUE SPRITE
+import tongueHit from "./img/frog_tongue.png";
+import tongueMiss from "./img/frog_tongue_miss.png";
+const spriteTongueHit = new Sprite(tongueHit, 1, 1);
+const spriteTongueMiss = new Sprite(tongueMiss, 1, 1);
+let spriteTongue = spriteTongueHit;
+const InitTongue = (tongue = spriteTongue) => {  
+  tongue.Stop();
+  tongue.x = TONGUE_X;
+  tongue.y = TONGUE_Y;  
+};
+InitTongue(spriteTongueHit);
+InitTongue(spriteTongueMiss);
+
+// TONGUE BODY SPRITES
+import tongueHitBody from "./img/frog_tongue_body.png";
+import tongueMissBody from "./img/frog_tongue_body_miss.png";
+const spritesTongueHitBody = [
+  new Sprite(tongueHitBody, 1, 1),
+  new Sprite(tongueHitBody, 1, 1)
+];
+const spritesTongueMissBody = [
+  new Sprite(tongueMissBody, 1, 1),
+  new Sprite(tongueMissBody, 1, 1)
+];
+let spritesTongueBody = spritesTongueHitBody;
+const InitTongueBody = (tongueBody = spritesTongueBody) => {
+  tongueBody.map((spriteTongueBody, i) => {
+    spriteTongueBody.Stop();
+    spriteTongueBody.x = TONGUE_X;
+    spriteTongueBody.y = TONGUE_BODY_Y + TONGUE_BODY_HEIGHT * i;
+  });
+};
+InitTongueBody(spritesTongueHitBody);
+InitTongueBody(spritesTongueMissBody);
+
+// BUG SPRITES
 import fly from "./img/fly.png";
 import bee from "./img/bee.png";
-const flySprites = [
+const bugSprites = [
   new Sprite(fly, 1, 1),
   new Sprite(bee, 2, 1),
   new Sprite(fly, 1, 1),
   new Sprite(fly, 1, 1)
 ];
 
-const TongueStretchedHit = () => {    
-  isTongueStretchedHit = true;
-  spriteTongue.reset();
-  spritesTongueBody.map(spriteTongueBody => {
-    spriteTongueBody.reset();
-  });
+const TongueStretchedHit = (isHit = true) => {
+  if (!isTongueStretched) {    
+    isTongueStretchedHit = isHit;
+    
+    if (isHit) {
+      spriteTongue = spriteTongueHit;
+      spritesTongueBody = spritesTongueHitBody;
+    }
+    else {
+      spriteTongue = spriteTongueMiss;
+      spritesTongueBody = spritesTongueMissBody;
+    }
+
+    spriteTongue.reset();
+    spritesTongueBody.map(spriteTongueBody => {
+      spriteTongueBody.reset();
+    });
+    isTongueStretched = true;
+  }
 };
 
 track.Events.on("Hit", e => {
   TongueStretchedHit();
 
   let bugX = 10;
-  flySprites.map((sprite, i, flySprites) => {
+  bugSprites.map((sprite, i, bugSprites) => {
     if (e.detail == bugX) {
       (sprite => sprite.Stop());
-      flySprites[i].Stop();
+      bugSprites[i].Stop();
     }
     bugX += 2;
   });
 });
 
 track.Events.on("Miss", e => {  
+  TongueStretchedHit(false);
   console.log("Frog tongue stretched out, but nothing happened!");
 });
 
@@ -78,25 +114,25 @@ export const Start = () => {
   
   spriteThing.reset();  
   
-  flySprites.map(sprite => {
+  bugSprites.map(sprite => {
     sprite.reset();
   });
 
   let bugX = 10;
-  flySprites.map(sprite => {
+  bugSprites.map(sprite => {
     sprite.x = bugX * 20;
     bugX += 2;
   });
 
   addUpdate(e => {
-    if (isTongueStretchedHit && spriteTongue.y > TONGUE_EXTENDS_Y) {
+    if (isTongueStretched && spriteTongue.y > TONGUE_EXTENDS_Y) {
       spriteTongue.y -= TONGUE_EXTENDS_Y_SPEED;
       spritesTongueBody.map(spriteTongueBody => {
         spriteTongueBody.y -= TONGUE_BODY_EXTENDS_Y_SPEED;
       });
     }
     else {
-      isTongueStretchedHit = false;
+      isTongueStretched = false;
       if (spriteTongue.y < TONGUE_Y) {
         spriteTongue.y += TONGUE_EXTENDS_Y_SPEED;
         spritesTongueBody.map(spriteTongueBody => {
@@ -113,7 +149,7 @@ export const Start = () => {
   });
 
   addUpdate(e => {
-    flySprites.map(sprite => {
+    bugSprites.map(sprite => {
       //needs to move 20 per second
       sprite.x -= (e.detail / timePerFrame) * 20;    
     });
