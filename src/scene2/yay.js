a3d3ab03a3324382d48c8add329720167e581fbb
Christer Kaitila <breakdance@mcfunkypants.com>
2021-04-08 14:08:16 -0700

    yay timings adjusted

--- src/scene2/yay.js
+++ src/scene2/yay.js
@@ -2,16 +2,21 @@ import Sprite from "../scripts/Sprite";
 import yaySprite from "./img/yay.png";
 import { addUpdate } from "../scripts/mainLoop";
 
-const MS_PER_YAY = 1000;
+const MS_PER_YAY = 500;
+const MS_PER_HIDE = 2500;
 
 export class Yay {
 	
     spr;
     nextYay;
+    nextHide;
     
     constructor() {
-        this.spr = new Sprite(yaySprite, 1, 11, MS_PER_YAY/1000);
-        this.nextYay = 0;
+        this.spr = new Sprite(yaySprite, 1, 11, 0.3);
+        this.spr.x = -9999;
+        this.spr.y = -9999;
+        this.nextYay = performance.now() + MS_PER_HIDE;
+        this.nextHide = 0;
         addUpdate(this.update);
     }
 
@@ -20,10 +25,15 @@ export class Yay {
         // sadly this does not synch up perfectly to the sprite anim framerate...
         // it's one frame ahead or behind or something
         let now = performance.now();
+        if (now >= this.nextHide) {
+            this.spr.x = -9999;
+            this.spr.y = -9999;
+        }
         if (now >= this.nextYay) {
-            this.nextYay = now + MS_PER_YAY;
+            this.nextYay = now + MS_PER_YAY + MS_PER_HIDE;
+            this.nextHide = now + MS_PER_YAY;
             this.spr.x = Math.round(Math.random()*(320-80));
-            this.spr.y = Math.round(Math.random()*(180-16));
+            this.spr.y = Math.round(Math.random()*(100));//(180-16));
         }
 
 	};
