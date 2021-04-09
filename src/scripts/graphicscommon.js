62be6ef361f7adfc665f23ab1a90670530419f65
Gamkedo <chris@gamkedo.com>
2021-04-08 19:10:16 -0700

    First pass credits integrated! :)

--- src/scripts/graphicscommon.js
+++ src/scripts/graphicscommon.js
@@ -17,6 +17,13 @@ export const colorBlank = (fillColor) => {
   colorRect(0,0,canvas.width,canvas.height,fillColor);
 }
 
+export const creditsText = (startX,startY,stringList) => {
+  let skipY = 10;
+  for(let i=0; i<stringList.length; i++) {
+    colorText(stringList[i],startX, startY+i*skipY, "white");
+  }
+}
+
 function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
   ctx.fillStyle = fillColor;
   ctx.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
--- src/scripts/mainLoop.js
+++ src/scripts/mainLoop.js
@@ -3,7 +3,7 @@ var started = false;
 import { run as runGraphics } from "./graphicscommon";
 
 import { colorBlank as drawColor } from "./graphicscommon";
-import { colorText as drawText } from "./graphicscommon";
+import { creditsText as drawCredits } from "./graphicscommon";
 
 export const updateEvent = new EventChannel();
 
@@ -32,12 +32,47 @@ function update(progress) {
   updateEvent.emit("Update", { detail: progress });
 }
 
+let creditsList = [
+"PRESS ANY KEY TO RETURN",
+" ",
+        "ClayTaeto: Project lead, frog images, core functionality, animation loading system, scene diagrams, color palette, fly and bee logic, score panel",
+"Jeff \"Axphin\" Hanlon: 2 looping songs, joke bug boot animation, frog design update, graphic item separation (art pipeline), font selection and related integration, fly sprite animation, continue button art",
+"Randy Tan Shaoxian: Mute toggle, frog tongue tuning, fly eating and miss sounds, tongue changes based on collision, additional minor bug fixes",
+"Christer \"McFunkypants\" Kaitila: Tadpole scene (plus related art, functionality), victory voice sound",
+"Charlene A.: Bee animation sprite, case sensitive compatibility fix",
+"Ian Cherabier: Pause menu and related art/functionality",
+"Clemens Reil: Practice commit"];
+let creditsMaxCharWidthToWrap = 68;
+
+function wrapCredits() { // note: gets calling immediately after definition
+    let newCut = [];
+    let findEnd;
+    for(var i=0;i<creditsList.length;i++) {
+        while(creditsList[i].length > 0) {
+            findEnd = creditsMaxCharWidthToWrap;
+            if(creditsList[i].length > creditsMaxCharWidthToWrap) {
+                for(var ii=findEnd;ii>0;ii--) {
+                    if(creditsList[i].charAt(ii) == " ") {
+                        findEnd=ii;
+                        break;
+                    }
+                }
+            }
+            newCut.push(creditsList[i].substring(0, findEnd));
+            creditsList[i] = creditsList[i].substring(findEnd, creditsList[i].length);
+        }
+    }
+    creditsList = newCut;
+}
+wrapCredits(); // calling once right at start
+
+
 function draw() {
   // Draw the state of the world
   runGraphics();
   if(creditsDisplay) {
     drawColor("blue");
-    // drawText("bill", 50,50, "white");
+    drawCredits(7,15, creditsList);
   }
 }
 
